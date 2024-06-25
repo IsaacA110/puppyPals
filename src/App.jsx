62b7a5430d691a27c16c0a75cb2ed1a4f703dadf
IsaacA110 <isaacaranzaso@gmail.com>
2024-06-25 11:48:54 -0400

    assignment

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,34 @@ import "./App.css";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
+  console.log(featuredPup);
   console.log("puppyList: ", puppyList);
-
-  function handleClick() {
-    // some logic here
-  }
 
   return (
     <div className="App">
       {puppies.map((puppy) => {
         return (
-          <p onClick={handleClick} key={puppy.id}>
+          <p
+            onClick={() => {
+              console.log("puppy id: ", puppy.id);
+            }}
+            key={puppy.id}
+          >
             {puppy.name}
           </p>
         );
       })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
